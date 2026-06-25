b1e88fdccfd013181b5cc5b527588da06a2ba06a
Emma Davidson <davidsone381@gmail.com>
2026-06-25 15:34:09 +0100

    synching

--- public/main.js
+++ public/main.js
@@ -31,5 +31,70 @@ const colour = d3.scaleOrdinal()
         d3.select("#minFrequency").on("input", drawDashboard);
         d3.select("#diseaseSearch").on("input", drawDashboard);
 
-        d3.select
+        d3.select("#resetButton").on("click", () => {
+            d3.select("#stageFilter").property("value", "");
+            d3.select("#minFrequency").property("value", 1);
+            d3.select("#diseaseSearch").property("value", "");
+            drawDashboard();
+        });
+
     });
+
+    function drawDashboard() {
+        svg.selectAll("*").remove();
+
+        const container = document.querySelector(".main");
+        const width = container.clientWidth - 30;
+        const height = container.clientHeight - 80;
+        
+        svg.attr("width", width).attr("height", height);
+
+         const selectedStage = d3.select("#stageFilter").property("value");
+         const minFrequency = +d3.select("#minFrequency").property("value");
+         const diseaseSearch = d3.select("#diseaseSearch").property("value").trim().toUpperCase();
+
+         d3.select("#freqLabel").text(minFrequency.toLocaleString());
+
+         let data = allData.filter (d=> d.count >=minFrequency);
+
+         if(selectedStage != "all") {
+            data = data.filter(d => d.Stage === selectedStage);
+
+         }
+
+         if(searchTerm !== ""){
+            data = data.filter(d =>
+                d.source.toUpperCase().includes(searchTerm) ||
+                d.Target.toUpperCase().includes(searchTerm)
+            );
+         }
+
+         const nodesMap = new Map();
+
+         data.forEach(d => {
+            nodesMap.set(d.source, {name : d.source});
+            nodesMap.set(d.Target, {name : d.Target});
+         });
+
+         const graph = {
+            nodes: Array.from(nodesMap.values()),
+            links: data.map(d => ({
+                source: d.source,
+                target: d.Target,
+                value: d.count,
+                stage: d.Stage
+
+            }))
+         };
+
+         if (graph.nodes.length === 0 || graph.links.length === 0) {
+            svg.append("text")
+                .attr("x", width / 2)
+                .attr("y", height / 2)
+                .attr("text-anchor", "middle")
+                .text("No data available for the selected filters.");
+                updateInsights([]);
+                return;
+
+         }
+        const
